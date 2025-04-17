af666b21d1e2a5dad3667063abc672adcc8d4e27
dishashetty546 <dishashetty546@gmail.com>
2025-04-17 20:46:01 +0530

    Using multiple models to train training dataset

--- Recommendation.py
+++ Recommendation.py
@@ -1,3 +1,4 @@
+from calendar import month_name
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
@@ -13,6 +14,42 @@ df = pd.read_csv("Datasets/Training.csv")
 
 X= df.drop("prognosis",axis=1)     # all input columns
 y= df["prognosis"]                 #output column
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)  #split into 4 parts, 0.3 70% of data to X_train and 30% to X_test
+
+# X_train.shape,X_test.shape,y_train.shape,y_test.shape        #check for size 
+
+le= LabelEncoder()
+le.fit(y)
+Y= le.transform(y)
+print(Y)       #string to integer
+
+#Training top models
+from sklearn import make_classification
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score,classification_report, confusion_matrix
+import numpy as np
+
+#all these models are stored in a dictionary
+models={
+    "SVC":SVC(kernel='linear',probability=True),
+    "RandomForest":RandomForestClassifier(n_estimators=100,random_state=42),
+    "GradientBoosting":GradientBoostingClassifier(n_estimators=100,random_state=42),
+    "Kneighbors":KNeighborsClassifier(n_neighbors=5),
+    "MultinomialNB":MultinomialNB()
+}
+for model_name, model in models.items():
+    #train model
+    model.fir(X_train,y_train)
+
+    #test model
+    predictions= model.predict(X_test)
+
+
+
+
 
 
 
